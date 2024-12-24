d4ec3371b6d083f534b8e432cf25d7fc13ead367
Brad Powell <bpowell461@gmail.com>
2024-12-23 20:00:39 -0600

    Assignment 4 commit

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -7,6 +7,7 @@
 #define DEBUG_LOG(msg,...)
 //#define DEBUG_LOG(msg,...) printf("threading: " msg "\n" , ##__VA_ARGS__)
 #define ERROR_LOG(msg,...) printf("threading ERROR: " msg "\n" , ##__VA_ARGS__)
+#define IS_ERROR(x) ((x)!= 0)
 
 void* threadfunc(void* thread_param)
 {
@@ -14,6 +15,27 @@ void* threadfunc(void* thread_param)
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+
+    usleep(thread_func_args->wait_to_obtain_ms * 1000);
+
+    thread_func_args->thread_complete_success = false;
+
+    if (IS_ERROR(pthread_mutex_lock(thread_func_args->mutex)))
+    {
+        ERROR_LOG("Failed to lock mutex");
+        return thread_param;
+    }
+
+    usleep(thread_func_args->wait_to_release_ms * 1000);
+
+    if(IS_ERROR(pthread_mutex_unlock(thread_func_args->mutex)))
+    {
+        ERROR_LOG("Failed to unlock mutex");
+        return thread_param;
+    }
+
+    thread_func_args->thread_complete_success = true;
     return thread_param;
 }
 
@@ -28,6 +50,20 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+
+    struct thread_data* thread_data = malloc(sizeof(struct thread_data));
+    if (NULL == thread_data)
+    {
+        ERROR_LOG("Failed to allocate memory for thread_data");
+        return false;
+    }
+
+    thread_data->mutex = mutex;
+    thread_data->wait_to_obtain_ms = wait_to_obtain_ms;
+    thread_data->wait_to_release_ms = wait_to_release_ms;
+
+    int rc = pthread_create(thread, NULL, threadfunc, (void*)thread_data);
+
+    return rc == 0;
 }
 
